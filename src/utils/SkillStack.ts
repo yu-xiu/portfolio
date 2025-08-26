927248a9614de3d78c04d9810849f2ce52387959
tiffany-xiu <xiuyutiffany@gmail.com>
2025-08-25 23:16:34 -0700

    edit skillstack

--- src/utils/SkillStack.ts
+++ src/utils/SkillStack.ts
@@ -37,9 +37,27 @@ import {
     GSAPIcon,
     FramerMotionIcon,
     BunIcon,
-    RIcon
+    RIcon,
+    googlecolab,
+    scikitlearn,
+    jupyter,
+    pytorch,
+    gymnasium,
+    ray,
+    spacy
 } from "../assets/Icons";
 import { GithubIcon } from "../assets/SVGs";
+
+export const MLFramework = [
+    { name : "CoLaboratory", icon: googlecolab, link: "https://colab.research.google.com/"},
+    { name : "Jupyter", icon: jupyter, link: "https://jupyter.org/"},
+    { name : "Scikit-learn", icon: scikitlearn, link: "https://scikit-learn.org/stable/"},
+    { name : "PyTorch", icon: pytorch, link: "https://pytorch.org/"},
+    { name : "Gymnasium", icon : gymnasium, link: "https://gymnasium.farama.org/index.html"},
+    { name : "Ray", icon: ray, link: "https://docs.ray.io/en/latest/rllib/rllib-algorithms.html"},
+    { name : "spaCy", icon: spacy, link: "https://spacy.io/"}
+
+];
 
 export const FrontendStack = [
     {
@@ -63,40 +81,40 @@ export const FrontendStack = [
         link: "https://www.typescriptlang.org/",
     },
     { name: "React.js", icon: ReactIcon, link: "https://react.dev/" },
-    { name: "Next.js", icon: NextIcon, link: "https://nextjs.org/" },
+    // { name: "Next.js", icon: NextIcon, link: "https://nextjs.org/" },
     { name: "Astro", icon: AstroIcon, link: "https://astro.build/" },
     { name: "Tailwind", icon: TailwindIcon, link: "https://tailwindcss.com/" },
-    {
-        name: "TS Query",
-        icon: ReactQueryIcon,
-        link: "https://tanstack.com/query/latest/",
-    },
-    { name: "GSAP", icon: GSAPIcon, link: "https://gsap.com/" },
-    { name: "Motion", icon: FramerMotionIcon, link: "https://www.framer.com/motion/" },
-    { name: "Redux", icon: ReduxIcon, link: "https://redux.js.org/" },
-    {
-        name: "Zustand",
-        icon: ZustandIcon,
-        link: "https://zustand-demo.pmnd.rs/",
-    },
-    { name: "D3.js", icon: D3JsIcon, link: "https://d3js.org/" },
-    { name: "Preact.js", icon: PreactIcon, link: "https://preactjs.com/" },
-    {
-        name: "Nanostores",
-        icon: NanostoresIcon,
-        link: "https://github.com/nanostores/nanostores",
-    },
+    // {
+    //     name: "TS Query",
+    //     icon: ReactQueryIcon,
+    //     link: "https://tanstack.com/query/latest/",
+    // },
+    // { name: "GSAP", icon: GSAPIcon, link: "https://gsap.com/" },
+    // { name: "Motion", icon: FramerMotionIcon, link: "https://www.framer.com/motion/" },
+    // { name: "Redux", icon: ReduxIcon, link: "https://redux.js.org/" },
+    // {
+    //     name: "Zustand",
+    //     icon: ZustandIcon,
+    //     link: "https://zustand-demo.pmnd.rs/",
+    // },
+    // { name: "D3.js", icon: D3JsIcon, link: "https://d3js.org/" },
+    // { name: "Preact.js", icon: PreactIcon, link: "https://preactjs.com/" },
+    // {
+    //     name: "Nanostores",
+    //     icon: NanostoresIcon,
+    //     link: "https://github.com/nanostores/nanostores",
+    // },
 ];
 export const BackendStack = [
     { name: "Node.js", icon: NodeIcon, link: "https://nodejs.org/" },
-    { name: "Express.js", icon: ExpressIcon, link: "https://expressjs.com/" },
+    // { name: "Express.js", icon: ExpressIcon, link: "https://expressjs.com/" },
     { name: "MongoDB", icon: MongoIcon, link: "https://www.mongodb.com/" },
     {
         name: "Firebase",
         icon: FirebaseIcon,
         link: "https://firebase.google.com/",
     },
-    { name: "Edge Store", icon: EdgeStoreIcon, link: "https://edgestore.dev/" },
+    // { name: "Edge Store", icon: EdgeStoreIcon, link: "https://edgestore.dev/" },
     { name: "MySQL", icon: MysqlIcon, link: "https://www.mysql.com/" },
     { name: "PHP", icon: PhpIcon, link: "https://www.php.net/" },
     {
@@ -109,14 +127,15 @@ export const BackendStack = [
         icon: GdriveIcon,
         link: "https://developers.google.com/drive",
     },
-    {
-        name: "Auth.js",
-        icon: NextAuthIcon,
-        link: "https://authjs.dev/",
-    },
+    // {
+    //     name: "Auth.js",
+    //     icon: NextAuthIcon,
+    //     link: "https://authjs.dev/",
+    // },
 ];
 
 export const ProgrammingStack = [
+    { name: "Python", icon: PythonIcon, link: "https://www.python.org/" },
     {
         name: "JavaScript",
         icon: JsIcon,
@@ -127,19 +146,19 @@ export const ProgrammingStack = [
         icon: TsIcon,
         link: "https://www.typescriptlang.org/",
     },
-    { name: "Python", icon: PythonIcon, link: "https://www.python.org/" },
+    
     { name: "C++", icon: CppIcon, link: "https://isocpp.org/" },
     { name: "Java", icon: JavaIcon, link: "https://www.oracle.com/java/" },
     { name: "R", icon: RIcon, link: "https://www.r-project.org/" },
 ];
 
 export const ToolsStack = [
-    { name: "NPM", icon: NpmIcon, link: "https://www.npmjs.com/" },
-    { name: "Bun", icon: BunIcon, link: "https://bun.sh/" },
-    { name: "Git", icon: GitIcon, link: "https://git-scm.com/" },
+    // { name: "NPM", icon: NpmIcon, link: "https://www.npmjs.com/" },
+    // { name: "Bun", icon: BunIcon, link: "https://bun.sh/" },
+    // { name: "Git", icon: GitIcon, link: "https://git-scm.com/" },
     { name: "GitHub", icon: GithubIcon, link: "https://github.com/" },
     { name: "Vite.js", icon: ViteIcon, link: "https://vitejs.dev/" },
-    { name: "Socket.io", icon: SocketIoIcon, link: "https://socket.io/" },
+    // { name: "Socket.io", icon: SocketIoIcon, link: "https://socket.io/" },
     { name: "Netlify", icon: NetlifyIcon, link: "https://www.netlify.com/" },
     { name: "Vercel", icon: VercelIcon, link: "https://vercel.com/" },
     {
@@ -149,3 +168,5 @@ export const ToolsStack = [
     },
     { name: "Figma", icon: FigmaIcon, link: "https://www.figma.com/" },
 ];
+
+

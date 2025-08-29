296fa98394f3627f6d474768905e19eba59fc65f
tiffany-xiu <xiuyutiffany@gmail.com>
2025-08-28 23:43:54 -0700

    add skills

--- src/utils/SkillStack.ts
+++ src/utils/SkillStack.ts
@@ -38,135 +38,95 @@ import {
     FramerMotionIcon,
     BunIcon,
     RIcon,
-    googlecolab,
-    scikitlearn,
-    jupyter,
-    pytorch,
-    gymnasium,
-    ray,
-    spacy
+    Bitbucket,
+    HuggingFace,
+    C,
+    MatLab,
+    Googlecolab,
+    Scikitlearn,
+    Jupyter,
+    Pytorch,
+    Gymnasium,
+    Ray,
+    Spacy,
+    Docker,
+    Googlecloud,
+    Aws,
+    Circleci,
+    LISP,
+    Latex,
+    TensorFlow,
+    Simulink,
+    AndroidStudio
 } from "../assets/Icons";
 import { GithubIcon } from "../assets/SVGs";
 
 export const MLFramework = [
-    { name : "CoLaboratory", icon: googlecolab, link: "https://colab.research.google.com/"},
-    { name : "Jupyter", icon: jupyter, link: "https://jupyter.org/"},
-    { name : "Scikit-learn", icon: scikitlearn, link: "https://scikit-learn.org/stable/"},
-    { name : "PyTorch", icon: pytorch, link: "https://pytorch.org/"},
-    { name : "Gymnasium", icon : gymnasium, link: "https://gymnasium.farama.org/index.html"},
-    { name : "Ray", icon: ray, link: "https://docs.ray.io/en/latest/rllib/rllib-algorithms.html"},
-    { name : "spaCy", icon: spacy, link: "https://spacy.io/"}
+    { name: "CoLaboratory", icon: Googlecolab, link: "https://colab.research.google.com/"},
+    { name: "Jupyter", icon: Jupyter, link: "https://jupyter.org/"},
+    { name: "Scikit-learn", icon: Scikitlearn, link: "https://scikit-learn.org/stable/"},
+    { name: "TensorFlow", icon: TensorFlow, link: "https://www.tensorflow.org/"},
+    { name: "PyTorch", icon: Pytorch, link: "https://pytorch.org/"},
+    { name: "Gymnasium", icon : Gymnasium, link: "https://gymnasium.farama.org/index.html"},
+    { name: "Ray", icon: Ray, link: "https://docs.ray.io/en/latest/rllib/rllib-algorithms.html"},
+    { name: "spaCy", icon: Spacy, link: "https://spacy.io/"},
+    { name: "HuggingFace", icon: HuggingFace, link: "https://huggingface.co/"}
+
+];
+
+export const CloudDevOps = [
+    {name: "aws", icon: Aws, link: "https://aws.amazon.com/"},
+    {name: "Docker", icon: Docker, link: "https://www.docker.com/"},
+    {name: "GoogleCloud", icon: Googlecloud, link: "https://cloud.google.com/"},
+    { name: "Vercel", icon: VercelIcon, link: "https://vercel.com/" },
+    {name: "CircleCi", icon: Circleci, link: "https://circleci.com/"}
 
 ];
 
 export const FrontendStack = [
-    {
-        name: "HTML",
-        icon: HtmlIcon,
-        link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-    },
-    {
-        name: "CSS",
-        icon: CssIcon,
-        link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    },
-    {
-        name: "JavaScript",
-        icon: JsIcon,
-        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    },
-    {
-        name: "TypeScript",
-        icon: TsIcon,
-        link: "https://www.typescriptlang.org/",
-    },
+    { name: "HTML", icon: HtmlIcon, link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+    { name: "CSS", icon: CssIcon, link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+    { name: "JavaScript", icon: JsIcon, link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+    { name: "TypeScript", icon: TsIcon, link: "https://www.typescriptlang.org/" },
     { name: "React.js", icon: ReactIcon, link: "https://react.dev/" },
-    // { name: "Next.js", icon: NextIcon, link: "https://nextjs.org/" },
+    { name: "ReactNative", icon: ReactIcon, link: "https://react.dev/" },
     { name: "Astro", icon: AstroIcon, link: "https://astro.build/" },
     { name: "Tailwind", icon: TailwindIcon, link: "https://tailwindcss.com/" },
-    // {
-    //     name: "TS Query",
-    //     icon: ReactQueryIcon,
-    //     link: "https://tanstack.com/query/latest/",
-    // },
-    // { name: "GSAP", icon: GSAPIcon, link: "https://gsap.com/" },
-    // { name: "Motion", icon: FramerMotionIcon, link: "https://www.framer.com/motion/" },
-    // { name: "Redux", icon: ReduxIcon, link: "https://redux.js.org/" },
-    // {
-    //     name: "Zustand",
-    //     icon: ZustandIcon,
-    //     link: "https://zustand-demo.pmnd.rs/",
-    // },
-    // { name: "D3.js", icon: D3JsIcon, link: "https://d3js.org/" },
-    // { name: "Preact.js", icon: PreactIcon, link: "https://preactjs.com/" },
-    // {
-    //     name: "Nanostores",
-    //     icon: NanostoresIcon,
-    //     link: "https://github.com/nanostores/nanostores",
-    // },
+    
 ];
+
 export const BackendStack = [
     { name: "Node.js", icon: NodeIcon, link: "https://nodejs.org/" },
-    // { name: "Express.js", icon: ExpressIcon, link: "https://expressjs.com/" },
     { name: "MongoDB", icon: MongoIcon, link: "https://www.mongodb.com/" },
-    {
-        name: "Firebase",
-        icon: FirebaseIcon,
-        link: "https://firebase.google.com/",
-    },
-    // { name: "Edge Store", icon: EdgeStoreIcon, link: "https://edgestore.dev/" },
+    { name: "Firebase", icon: FirebaseIcon, link: "https://firebase.google.com/" },
     { name: "MySQL", icon: MysqlIcon, link: "https://www.mysql.com/" },
     { name: "PHP", icon: PhpIcon, link: "https://www.php.net/" },
-    {
-        name: "REST API",
-        icon: RestApiIcon,
-        link: "https://developer.mozilla.org/en-US/docs/Glossary/REST",
-    },
-    {
-        name: "Drive API",
-        icon: GdriveIcon,
-        link: "https://developers.google.com/drive",
-    },
-    // {
-    //     name: "Auth.js",
-    //     icon: NextAuthIcon,
-    //     link: "https://authjs.dev/",
-    // },
+    { name: "REST API", icon: RestApiIcon, link: "https://developer.mozilla.org/en-US/docs/Glossary/REST" },
+    { name: "Drive API", icon: GdriveIcon, link: "https://developers.google.com/drive" },
 ];
 
 export const ProgrammingStack = [
     { name: "Python", icon: PythonIcon, link: "https://www.python.org/" },
-    {
-        name: "JavaScript",
-        icon: JsIcon,
-        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    },
-    {
-        name: "TypeScript",
-        icon: TsIcon,
-        link: "https://www.typescriptlang.org/",
-    },
-    
-    { name: "C++", icon: CppIcon, link: "https://isocpp.org/" },
     { name: "Java", icon: JavaIcon, link: "https://www.oracle.com/java/" },
+    { name: "JavaScript", icon: JsIcon, link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+    { name: "TypeScript", icon: TsIcon, link: "https://www.typescriptlang.org/" },
+    { name: "MATLAB", icon: MatLab, link: "https://www.mathworks.com/products/matlab.html"},
+    { name: "C++", icon: CppIcon, link: "https://isocpp.org/" },
+    { name: "C", icon: C, link: "https://www.c-language.org/"},
+    { name: "Lisp", icon: LISP, link: "https://lisp-lang.org/"},
     { name: "R", icon: RIcon, link: "https://www.r-project.org/" },
 ];
 
 export const ToolsStack = [
-    // { name: "NPM", icon: NpmIcon, link: "https://www.npmjs.com/" },
-    // { name: "Bun", icon: BunIcon, link: "https://bun.sh/" },
-    // { name: "Git", icon: GitIcon, link: "https://git-scm.com/" },
     { name: "GitHub", icon: GithubIcon, link: "https://github.com/" },
+    { name: "Bitbucket", icon: Bitbucket, link: "https://www.atlassian.com/software/bitbucket"},
     { name: "Vite.js", icon: ViteIcon, link: "https://vitejs.dev/" },
-    // { name: "Socket.io", icon: SocketIoIcon, link: "https://socket.io/" },
-    { name: "Netlify", icon: NetlifyIcon, link: "https://www.netlify.com/" },
-    { name: "Vercel", icon: VercelIcon, link: "https://vercel.com/" },
-    {
-        name: "VS Code",
-        icon: VsCodeIcon,
-        link: "https://code.visualstudio.com/",
-    },
+    { name: "NPM", icon: NpmIcon, link: "https://www.npmjs.com/" },
+    { name: "VS Code", icon: VsCodeIcon, link: "https://code.visualstudio.com/"},
+    { name: "AndroidStudio", icon: AndroidStudio, link: "https://developer.android.com/" },
     { name: "Figma", icon: FigmaIcon, link: "https://www.figma.com/" },
+    { name: "LaTex", icon: Latex, link: "https://www.latex-project.org/" },
+    { name: "Simulink", icon: Simulink, link: "https://www.mathworks.com/products/simulink.html" },
 ];
 
 
